7b284b81e564f4a1dc577e2086c61135203ac147
Glen Maddern <glenmaddern@gmail.com>
2016-09-05 10:06:42 +0800

    added a simple router

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { this.props.children }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import { render } from 'react-snapshot'
+import {render} from 'react-snapshot'
 import App from './App';
 import './index.css';
+import {Router, Route, browserHistory, IndexRoute, Link} from 'react-router'
+
+const Index = () => (
+  <div>
+    <h1>This is the index!!</h1>
+    <Link to="/login">Log in!</Link>
+    <br/>
+    <Link to="/about">About this site!</Link>
+  </div>
+)
+
+const Login = () => (
+  <div>
+    <form action="/admin">
+      <h1>This is a login form, not followed</h1>
+      <button type="submit">Go to Admin</button>
+    </form>
+    <Link to="/">Home</Link>
+    <br/>
+    <Link to="/about">About this site!</Link>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h1>About this site</h1>
+    <p>This should all be snapshotted.</p>
+    <Link to="/">Home</Link>
+    <br/>
+    <Link to="/login">Log in!</Link>
+  </div>
+)
 
 render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Index}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/about" component={About}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
